ff6d66b898cf7d05ddc615a55f1ada7c2c411076
dann <austindan168@gmail.com>
2025-01-14 08:57:33 +0300

    first flask app

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile of {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port = 55555, debug = True)
